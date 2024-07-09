a0ba34cf88bb15fc13cc1246cacfbd3903bf7a8a
HangNT <HangNT169@fpt.edu.vn>
2024-07-10 01:54:47 +0700

    Init

---
+++ src/ptpm_pro1041_su24_bl2/PTPM_PRO1041_SU24_BL2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ptpm_pro1041_su24_bl2;
+
+/**
+ *
+ * @author hangnt
+ */
+public class PTPM_PRO1041_SU24_BL2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
